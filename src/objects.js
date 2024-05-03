9c426f9e17ed8fcf95c73ef9e787dceafd3470bb
Craig <craigthompson85@gmail.com>
2024-05-03 10:30:35 -0600

    Completed objects.js section of assessment

--- src/objects.js
+++ src/objects.js
@@ -7,13 +7,19 @@
 // { firstName: 'Dame', lastName: 'Aylin', location: 'Reithwin' }
 //
 // Ex.:
-//   getNames([
-//     { firstName: 'Gale', lastName: 'Dekarios', location: 'Waterdeep' },
-//     { firstName: 'Wyll', lastName: 'Ravengard', location: "Baldur's Gate" },
-//     { firstName: 'Karlach', lastName: 'Cliffgate', location: 'Avernus' }
-//   ]);
+// getNames([
+//   { firstName: 'Gale', lastName: 'Dekarios', location: 'Waterdeep' },
+//   { firstName: 'Wyll', lastName: 'Ravengard', location: "Baldur's Gate" },
+//   { firstName: 'Karlach', lastName: 'Cliffgate', location: 'Avernus' }
+// ]);
 //   => ['Gale Dekarios', 'Wyll Ravengard', 'Karlach Cliffgate'];
-function getNames(people) {}
+function getNames(people) {
+  const newPeople = [];
+  people.forEach((person) =>
+    newPeople.push(person.firstName + " " + person.lastName)
+  );
+  return newPeople;
+}
 
 // Given an object representing a person, return their full name (first name and last name).
 // You MUST use object destructuring in your solution.
@@ -24,7 +30,10 @@ function getNames(people) {}
 // Ex.:
 //   getName({ firstName: 'Gale', lastName: 'Dekarios', location: 'Waterdeep' });
 //   => 'Gale Dekarios'
-function getNameUsingDestructuring(person) {}
+function getNameUsingDestructuring(person) {
+  const { firstName, lastName } = person;
+  return firstName + " " + lastName;
+}
 
 // Given an array of objects representing people, return a new array of the
 // people matching the given location.
@@ -43,7 +52,9 @@ function getNameUsingDestructuring(person) {}
 //     { firstName: 'Wyll', lastName: 'Ravengard', location: "Baldur's Gate" },
 //     { firstName: 'Astarion', lastName: 'Ancunin', location: "Baldur's Gate" }
 //   ];
-function getPeopleByLocation(people, location) {}
+function getPeopleByLocation(people, location) {
+  return people.filter((person) => person.location === location);
+}
 
 // Translate a phrase to pirate talk.
 //
@@ -58,21 +69,28 @@ function getPeopleByLocation(people, location) {}
 //   translateToPirateTalk('excuse me sir where is the restroom');
 //   => 'avast me matey where be the head'
 const EN_PIRATE_LOOKUP = {
-  excuse: 'avast',
-  sir: 'matey',
-  is: 'be',
-  restroom: 'head',
-  student: 'swabbie',
-  friend: 'matey',
-  restaurant: 'galley',
-  your: 'yer',
-  are: 'be',
-  my: 'me',
-  hotel: 'fleabag inn',
-  hello: 'ahoy',
+  excuse: "avast",
+  sir: "matey",
+  is: "be",
+  restroom: "head",
+  student: "swabbie",
+  friend: "matey",
+  restaurant: "galley",
+  your: "yer",
+  are: "be",
+  my: "me",
+  hotel: "fleabag inn",
+  hello: "ahoy",
 };
 
-function translateToPirateTalk(phrase) {}
+function translateToPirateTalk(phrase) {
+  const arrWords = phrase.split(" ");
+  return arrWords
+    .map((word) => {
+      return EN_PIRATE_LOOKUP[`${word}`] ? EN_PIRATE_LOOKUP[`${word}`] : word;
+    })
+    .join(" "); // Creates a string by concatenating all of the elements in the mapped array
+}
 
 // Return the number of occurrences of each word in a string.
 // This function doesn't handle punctuation and is case-sensitive, so you can
@@ -81,7 +99,14 @@ function translateToPirateTalk(phrase) {}
 // Ex.:
 //   wordCount('hello world')
 //   => { hello: 1, world: 1 }
-function wordCount(str) {}
+function wordCount(str) {
+  const countOfWords = {};
+  str.split(" ").forEach((word) => {
+    const count = countOfWords[`${word}`] ? countOfWords[`${word}`] + 1 : 1;
+    countOfWords[`${word}`] = count;
+  });
+  return countOfWords;
+}
 
 // Given an object representing a bug, return true if the given bug is
 // available in the given month.
@@ -103,7 +128,9 @@ function wordCount(str) {}
 //     }
 //   }, 1);
 //   => true
-function isBugAvailable(bug, month) {}
+function isBugAvailable(bug, month) {
+  return bug.availability.months.includes(month);
+}
 
 // Given an array of objects representing bugs, return an object that'll be
 // used to build a calendar. The keys of the object should be the months of the
@@ -146,7 +173,16 @@ function isBugAvailable(bug, month) {}
 //     12: [],
 //   }
 
-function buildBugHuntCalendar(bugs) {}
+function buildBugHuntCalendar(bugs) {
+  const calendar = {};
+  for (let i = 1; i <= 12; i++) calendar[`${i}`] = []; // Populate calendar with 12 months
+  for (const bug in bugs) {
+    bugs[`${bug}`].availability.months.forEach((month) =>
+      calendar[`${month}`].push(bugs[`${bug}`].name)
+    );
+  }
+  return calendar;
+}
 
 export {
   buildBugHuntCalendar,
